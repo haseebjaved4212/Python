5fe1eb002c191d975f68cb730a788a0692c4ee29
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-01 08:52:44 +0500

    refactor: remove old variable and first.py files; add new implementations for variables and data types

---
+++ 03_Data-Types.py
@@ -0,0 +1,11 @@
+a = 1 # a is integer
+b = 2.5 # b is floating point number
+c = "Hello" # c is string 
+d = True # d is boolean
+e = None # e is NoneType 
+
+print(a, type(a))
+print(b, type(b))
+print(c, type(c))
+print(d, type(d))
+print(e, type(e))
