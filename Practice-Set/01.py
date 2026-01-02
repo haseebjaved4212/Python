1cae19645223808bfccfa3cf88ebf0a04cc478fd
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-02 10:27:50 +0500

    add remainder calculation to 01.py

--- Practice-Set/01.py
+++ Practice-Set/01.py
@@ -4,3 +4,8 @@ a= 2
 b = 3
 print("The sum of a and b  is:", a + b)
 
+# 2. Write a python program to find remainder when a number is divided by z.
+
+a = 10
+b = 3
+print("The remainder when a is divided by b is:", a % b)
