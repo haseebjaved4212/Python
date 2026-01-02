d62d9bd85783d23d7eaf072cd2aa35aad4042054
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-02 10:42:44 +0500

    refactor 01.py to improve structure and readability by adding comments and organizing code sections

--- Practice-Set/01.py
+++ Practice-Set/01.py
@@ -9,3 +9,28 @@ print("The sum of a and b  is:", a + b)
 a = 10
 b = 3
 print("The remainder when a is divided by b is:", a % b)
+
+# 3. Check the type of variable assigned using input () function.
+
+a = input("Enter a value: ")
+print("Type of a:", type(a))
+
+# 4. Use comparison operator to find out whether ‘a’ given variable a is greater than
+# ‘b’ or not. Take a = 34 and b = 80
+
+a =int(input("Enter value for a: "))
+b =int(input("Enter value for b: "))
+print("Is 'a' greater than 'b'?", a > b)
+
+
+# 5. Write a python program to find an average of two numbers entered by the user.
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print("The average of a and b is:", (a + b) / 2)
+
+
+# 6. Write a python program to calculate the square of a number entered by the user.
+
+num = int(input("Enter a number to find its square: "))
+square = num ** 2
+print("The square of", num, "is", square)
