e77eeb3ff8019cc7a6e0f5d4332c1bb2ed53d30c
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-05 11:30:16 +0500

    refactor 02.py to uncomment letter template code and improve string detection functionality

--- Practice-Set/02.py
+++ Practice-Set/02.py
@@ -6,15 +6,23 @@
 
 # 2. Write a program to fill in a letter template given below with name and date.
 
-""" letter = '''Dear <|Name|>,
-Greetings from ABC coding house. I am happy to tell you about the selection.
-You are selected!
-Have a great day ahead!
-Thanks and regards,
-Bill
-Date: <|Date|>'''
+# """ letter = '''Dear <|Name|>,
+# Greetings from ABC coding house. I am happy to tell you about the selection.
+# You are selected!
+# Have a great day ahead!
+# Thanks and regards,
+# Bill
+# Date: <|Date|>'''
 
-name = input("Enter your name: ")
-date = input("Enter date: ")
+# name = input("Enter your name: ")
+# date = input("Enter date: ")
 
-print(letter.replace("<|Name|>", name).replace("<|Date|>", date))"""
+# print(letter.replace("<|Name|>", name).replace("<|Date|>", date))"""
+
+
+# 3. Write a python program to detect double spaces in a string.
+
+string = input("Enter a string: ")
+if "  " in string:  print("Double spaces detected.")
+else:   print("No double spaces detected.")
+
