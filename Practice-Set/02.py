d1046483292ad2028b629cf0e5fdd1870854db74
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-05 11:38:51 +0500

    uncomment code to enable user input for greeting and letter template functionality

--- Practice-Set/02.py
+++ Practice-Set/02.py
@@ -1,30 +1,29 @@
 # 1. Write a python program to display a user entered name followed by Good Afternoon using input () function.
 
-# name = input("Enter your name: ")
-# print("Good Afternoon", name)
+name = input("Enter your name: ")
+print("Good Afternoon", name)
 
 
 # 2. Write a program to fill in a letter template given below with name and date.
 
-# """ letter = '''Dear <|Name|>,
-# Greetings from ABC coding house. I am happy to tell you about the selection.
-# You are selected!
-# Have a great day ahead!
-# Thanks and regards,
-# Bill
-# Date: <|Date|>'''
+letter = '''Dear <|Name|>,
+Greetings from ABC coding house. I am happy to tell you about the selection.
+You are selected!
+Have a great day ahead!
+Thanks and regards,
+Bill
+Date: <|Date|> '''
 
-# name = input("Enter your name: ")
-# date = input("Enter date: ")
+name = input("Enter your name: ")
+date = input("Enter date: ")
 
-# print(letter.replace("<|Name|>", name).replace("<|Date|>", date))"""
-
+print(letter.replace("<|Name|>", name).replace("<|Date|>", date)) 
 
 # 3. Write a python program to detect double spaces in a string.
 
-# string = input("Enter a string: ")
-# if "  " in string:  print("Double spaces detected.")
-# else:   print("No double spaces detected.")
+string = input("Enter a string: ")
+if "  " in string:  print("Double spaces detected.")
+else:   print("No double spaces detected.")
 
 
 # 5. Write a program to format the following letter using escape sequence characters.
