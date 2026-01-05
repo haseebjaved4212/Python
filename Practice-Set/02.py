1a55554260256bd96cf1af85d4715a1c7fb442ef
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-01-05 11:26:49 +0500

    add 02.py to implement user input for a greeting and a letter template

---
+++ Practice-Set/02.py
@@ -0,0 +1,20 @@
+# 1. Write a python program to display a user entered name followed by Good Afternoon using input () function.
+
+# name = input("Enter your name: ")
+# print("Good Afternoon", name)
+
+
+# 2. Write a program to fill in a letter template given below with name and date.
+
+""" letter = '''Dear <|Name|>,
+Greetings from ABC coding house. I am happy to tell you about the selection.
+You are selected!
+Have a great day ahead!
+Thanks and regards,
+Bill
+Date: <|Date|>'''
+
+name = input("Enter your name: ")
+date = input("Enter date: ")
+
+print(letter.replace("<|Name|>", name).replace("<|Date|>", date))"""
