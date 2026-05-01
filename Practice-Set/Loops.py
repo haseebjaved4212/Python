0cc9a11cdecd726c2f9a2a0ff5f56c05ba2e4386
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-05-01 21:26:12 +0500

    Comment out string reversal implementation and add notes for palindrome check

--- Practice-Set/Loops.py
+++ Practice-Set/Loops.py
@@ -17,8 +17,12 @@ for i in range(1, n + 1):
 
 # Q3: Reverse a String Without Using Built-in Functions
 
-a = input("Enter a string: ")
+# a = input("Enter a string: ")
 
-for i in range(len(a)-1, -1, -1):
-    print(a[i], end="")
+# for i in range(len(a)-1, -1, -1):
+#     print(a[i], end="")
+
+#  Q4 : Check if the String is Palindrome or not?
+# First of all, we need to understand what a palindrome is. A palindrome is a word, phrase, number, or other sequence of characters that reads the same forward and backward (ignoring spaces, punctuation, and capitalization).
+
 
