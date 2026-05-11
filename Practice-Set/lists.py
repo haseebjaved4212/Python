b2d6aebb3c50bf5cfadb194d96981fcf06e9c0bf
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-05-11 21:20:13 +0500

    Refactor code to find the second largest number in a list and its index

--- Practice-Set/lists.py
+++ Practice-Set/lists.py
@@ -1,12 +1,27 @@
 # Q1: Print the largest number in the list and its index.
 
+# l = [3, 5, 7, 2, 8, 1]
+# largest = l[0]
+# index = 0
+
+# for i in range(1, len(l)):
+#     if l[i] > largest:
+#         largest = l[i]  
+#         index = i
+# print(f"Largest number: {largest} at the Index: {index}")
+
+# Q2: Print the second largest number in the list and its index.
+
 l = [3, 5, 7, 2, 8, 1]
 largest = l[0]
+second_largest = l[0]
 index = 0
 
 for i in range(1, len(l)):
     if l[i] > largest:
-        largest = l[i]  
-        index = i
-print(f"Largest number: {largest} at the Index: {index}")
-
+        second_largest = largest
+        largest = l[i]
+        index = i 
+    elif l[i] > second_largest and l[i] != largest:
+        second_largest = l[i]
+print(f"Second largest number: {second_largest} at the Index: {index}")
