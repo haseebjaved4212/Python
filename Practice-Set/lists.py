094773fad22361ee0b88b741ddce332cc2989038
haseebjaved4212 <m.haseebjavedhj@gmail.com>
2026-05-13 12:21:57 +0500

    Remove implementation for finding the second largest number and its index; add check for sorted list

--- Practice-Set/lists.py
+++ Practice-Set/lists.py
@@ -12,16 +12,28 @@
 
 # Q2: Print the second largest number in the list and its index.
 
-l = [3, 5, 7, 2, 8, 1]
-largest = l[0]
-second_largest = l[0]
-index = 0
+# l = [3, 5, 7, 2, 8, 1]
+# largest = l[0]
+# second_largest = l[0]
+# index = 0
 
-for i in range(1, len(l)):
-    if l[i] > largest:
-        second_largest = largest
-        largest = l[i]
-        index = i 
-    elif l[i] > second_largest and l[i] != largest:
-        second_largest = l[i]
-print(f"Second largest number: {second_largest} at the Index: {l.index(second_largest)}")
+# for i in range(1, len(l)):
+#     if l[i] > largest:
+#         second_largest = largest
+#         largest = l[i]
+#         index = i 
+#     elif l[i] > second_largest and l[i] != largest:
+#         second_largest = l[i]
+# print(f"Second largest number: {second_largest} at the Index: {l.index(second_largest)}")
+
+
+#  Q3: check if the list is sorted or not.
+
+a = [1, 2, 6, 4, 5]
+
+for i in range(len(a) - 1):
+    if a[i] > a[i + 1]:
+        print("The list is not sorted.")
+        break
+else:
+    print("The list is sorted.")
